a8ee7080a020bacaf1b514266326d84c52f6b810
vanduc2012 <vanduc201296@gmail.com>
2020-03-09 00:47:37 +0700

    update model id Identify -> Auto

--- src/main/java/com/example/mta/demo/models/Contact.java
+++ src/main/java/com/example/mta/demo/models/Contact.java
@@ -21,7 +21,7 @@ public class Contact implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private Long id;
 
