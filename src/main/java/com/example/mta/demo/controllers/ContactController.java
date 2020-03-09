35ca7344d5b867e89dca0338dea5ebc5fc9bf132
vanduc2012 <vanduc201296@gmail.com>
2020-03-10 00:01:48 +0700

    update audit model, thêm create_at và update_at

--- src/main/java/com/example/mta/demo/controllers/ContactController.java
+++ src/main/java/com/example/mta/demo/controllers/ContactController.java
@@ -37,6 +37,7 @@ public class ContactController {
 
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Successfully retrieved list"),
+            @ApiResponse(code = 204, message = "No Content"),
             @ApiResponse(code = 401, message = "You are not authorized to view the resource"),
             @ApiResponse(code = 403, message = "Accessing the resource you were trying to reach is forbidden"),
             @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
